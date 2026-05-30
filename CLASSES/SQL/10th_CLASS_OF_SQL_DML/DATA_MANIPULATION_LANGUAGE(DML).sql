b61a4a9b0b782c8464d8af99d37c3852268bf840
iqbalmeerpa <iqbalasifmeerpa@gmail.com>
2026-05-30 22:39:45 +0500

    assignment and class code deployed related to DML

---
+++ CLASSES/SQL/10th_CLASS_OF_SQL_DML/DATA_MANIPULATION_LANGUAGE(DML).sql
@@ -0,0 +1,66 @@
+INSERT INTO production.categories(category_id,category_name)
+	VALUES(1,'Children Bicycles'),
+		  (2,'Comfort Bicycles'),
+		  (3,'Cruisers Bicycles')
+
+Select * from 
+
+Create table new_york_customers(
+
+
+----------------------------MERGE STATEMENT------------------------------------------
+MERGE target_table USING source_table
+ON merge_condition
+WHEN MATCHED
+    THEN update_statement
+WHEN NOT MATCHED
+    THEN insert_statement
+WHEN NOT MATCHED BY SOURCE
+    THEN DELETE;
+
+
+
+
+CREATE TABLE sales.category (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+INSERT INTO sales.category(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (2,'Comfort Bicycles',25000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',10000);
+
+
+CREATE TABLE sales.category_staging (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+
+INSERT INTO sales.category_staging(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',20000),
+    (5,'Electric Bikes',10000),
+    (6,'Mountain Bikes',10000);
+
+
+
+
+MERGE sales.category t 
+    USING sales.category_staging s
+ON (s.category_id = t.category_id)
+WHEN MATCHED
+    THEN UPDATE SET 
+        t.category_name = s.category_name,
+        t.amount = s.amount
+WHEN NOT MATCHED BY TARGET 
+    THEN INSERT (category_id, category_name, amount)
+         VALUES (s.category_id, s.category_name, s.amount)
+WHEN NOT MATCHED BY SOURCE 
+    THEN DELETE;
+
